84d92abf10b5aeb9dfb8e372a9bbf2531d2e4d16
Dave Taylor <tayloredwebsites@me.com>
2023-03-25 09:54:32 -0400

    refactor csv uploads

--- app/services/import_usda_csv_files.rb
+++ app/services/import_usda_csv_files.rb
@@ -14,27 +14,39 @@ class ImportUsdaCsvFiles
     @errors = []
   end
 
+
+  # method to do all of the uploads of the usda csv files to initialize the database
   def run()
 
-    # all of the uploads should be rerunnable.
-    # check to see if record exists by looking for the record based upon its primary specification fields (primary keys).
-    # If not found, add it, otherwise update all fields except the primary specification fields
+    # NOTE: all of the uploads should be rerunnable.
+    #   check to see if record exists by looking for the record based upon its primary specification fields (primary keys).
+    #   If not found, add it, otherwise update all fields except the primary specification fields
 
-    import_usda_categories()
-    import_wweia_categories()
+    import_csv_into_table('db/csv_uploads/food_category.csv', LookupTable, 'usda_cat', method(:set_food_category_fields) )
+    import_csv_into_table('db/csv_uploads/wweia_food_category.csv', LookupTable, 'wweia_cat', method(:set_wweia_category_fields) )
+
+
 
     return @report, @errors
-
   end
 
-  def import_usda_categories()
+  # method to read any csv import file into any table
+  #
+  # @param filename is the csv file to be uploaded (including path from rails root)
+  # @param model_clazz - the Model that the the fields are to be added/updated
+  # @param filter - currently the lookup table's lu_table field value to match
+  # @param set_fields is the callback method to properly update the model fields from the csv fields
+  # @return - none
+  # @example
+  #   see run() method for examples
+  def import_csv_into_table(filename, model_clazz, filter, set_fields)
     @report << ''
-    msg = "Start of Importing of USDA Categories"
-    Rails.logger.debug("*** #{msg}")
+    msg = "Start of Importing of #{model_clazz.class} table"
+    Rails.logger.debug("*** #{model_clazz.new.inspect}")
     @report << msg
-    start_rec_count = LookupTable.all.count
+    start_rec_count = model_clazz.all.count
     # read in usda categories into lookup tables:
-    filename = Rails.root.join('db/csv_uploads/food_category.csv')
+    filename = Rails.root.join(filename)
     chunk_size = 10
     chunk_num = 0
     options = { chunk_size: chunk_size } # {:key_mapping => {:unwanted_row => nil, :old_row_name => :new_name}}
@@ -43,105 +55,115 @@ class ImportUsdaCsvFiles
         Rails.logger.debug("@@@ read line #{row.inspect}")
         rec_num = chunk_num * chunk_size + ix + 1
         msg = ''
-        matching = LookupTable.where(lu_table: 'usda_cat', lu_code: row[:code])
+        matching = model_clazz.where(lu_table: filter, lu_code: row[:code])
         if matching.count == 1
           rec = matching.first
-          rec.lu_id = row[:id]
-          rec.lu_desc = row[:description]
-          if rec.changed
-            rec.save
-            if rec.errors.count > 0
-              msg = "Error writing USDA Category table row:#{rec_num} - #{rec.errors}"
-              @errors << err_str
-            else
-              msg = "Updated row:#{rec_num} - #{rec[:id]},#{rec[:lu_code]}, #{rec[:lu_desc]}"
-            end
-          else
-            # no need to update it, as it has not changed
-          end
+          msg, errors = set_fields.call(rec, row)
         else
-          rec = LookupTable.new(
-            lu_table: 'usda_cat',
-            lu_code: row[:code],
-            lu_id: row[:id],
-            lu_desc: row[:description],
-          )
-          rec.save
-          if rec.errors.count > 0
-            msg = "Error writing USDA Category table row:#{rec_num} - #{rec.errors}"
-            @errors << err_str
-          else
-            msg = "Added row:#{rec_num} - #{rec[:lu_code]}, #{rec[:lu_desc]}"
-          end
+          rec = LookupTable.new()
+          msg, errors = set_fields.call(rec, row)
         end
+        Rails.logger.debug("### rec: #{rec.inspect}")
+        Rails.logger.debug("### errors: #{errors.inspect}")
         @report << msg
+        @errors << errors if errors.present?
       end
       chunk_num += 1
     end
-    diff_num_recs = LookupTable.all.count - start_rec_count
-    msg = "# imported #{diff_num_recs} new records from USDA Categories"
+    diff_num_recs = model_clazz.all.count - start_rec_count
+    msg = "# imported #{diff_num_recs} new records from #{filename} into #{model_clazz.class}"
     Rails.logger.info("***#{msg}")
     @report << msg
-    Rails.logger.error("ERROR: #{@errors.inspect}") if @errors.count > 0
+    Rails.logger.error("ERRORs: #{@errors.inspect}") if @errors.count > 0
+    # Rails.logger.debug("### @report: #{@report.inspect}")
   end
 
-  def import_wweia_categories()
-    @report << ''
-    msg = "Start of Importing of WWEIA Categories"
-    Rails.logger.debug("*** #{msg}")
-    @report << msg
-    start_rec_count = LookupTable.all.count
-    # read in usda categories into lookup tables:
-    filename = Rails.root.join('db/csv_uploads/wweia_food_category.csv')
-    chunk_size = 10
-    chunk_num = 0
-    options = { chunk_size: chunk_size } # {:key_mapping => {:unwanted_row => nil, :old_row_name => :new_name}}
-    n = SmarterCSV.process(filename, options) do |array|
-      # we're passing a block in, to process each resulting hash / =row (the block takes array of hashes)
-      # when chunking is not enabled, there is only one hash in each array
-      # Rails.logger.debug("@@@ read chunk #{array.inspect}")
-      array.each_with_index do |row, ix|
-        Rails.logger.debug("@@@ read line #{row.inspect}")
-        rec_num = chunk_num * chunk_size + ix + 1
-        msg = ''
-        matching = LookupTable.where(lu_table: 'wweia_cat', lu_code: row[:wweia_food_category])
-        if matching.count == 1
-          rec = matching.first
-          rec.lu_desc = row[:wweia_food_category_description]
-          if rec.changed
-            rec.save
-            if rec.errors.count > 0
-              msg = "Error updating WWEIA Category table row:#{rec_num} - #{rec.errors}"
-              @errors << err_str
-            else
-              msg = "Updated row:#{rec_num} - #{rec[:lu_code]}, #{rec[:lu_desc]}"
-            end
-          else
-            # no need to update it, as it has not changed
-          end
-        else
-          rec = LookupTable.new(
-            lu_table: 'wweia_cat',
-            lu_code: row[:wweia_food_category],
-            lu_desc: row[:wweia_food_category_description]
-          )
-          rec.save
-          if rec.errors.count > 0
-            msg = "Error writing WWEIA Category table row:#{rec_num} - #{rec.errors}"
-            @errors << err_str
-          else
-            msg = "Added row:#{rec_num} - #{rec[:lu_code]}, #{rec[:lu_desc]}"
-          end
-        end
-        @report << msg
+
+  # callback method to set the food category entries in the Lookup Table
+  #
+  # @param rec is the current record in LookupTables
+  # @param row is the current row being read from the csv file
+  # @return - [ <the message to go in the report>, <the optional error message, or blank> ]
+  # @example
+  #   see run() method for examples
+  def set_food_category_fields(rec, row)
+    Rails.logger.debug "*** Food Category Record:"
+    Rails.logger.debug "*** rec: #{rec.inspect}"
+    Rails.logger.debug "*** row: #{row.inspect}"
+
+    err_str = msg = ""
+    # put in fields needed for both update and add
+    rec.lu_id = row[:id]
+    rec.lu_desc = row[:description]
+    # Update the record if it existed already
+    if rec.present? && rec.id.present?
+      if rec.lu_table != 'usda_cat' || rec.lu_code != row[:code].to_s
+        msg = "Error, trying to update an invalid record at Lookup Table id: #{rec.id}"
+        Rails.logger.error msg
+        Rails.logger.error "### row[:code]: #{row[:code].inspect} rec: #{rec.inspect}"
+        err_str = msg
+      elsif rec.changed
+        rec.save
+        msg = "Updated Category Lookup row.id:#{row[:id]} - #{rec[:id]},#{rec[:lu_code]}, #{rec[:lu_desc]}"
+      else
+        # no need to update it, as it has not changed
       end
-      chunk_num += 1
+    else
+      # new record, create it anew
+      rec.lu_table = 'usda_cat'
+      rec.lu_code = row[:code]
+      rec.save
+      msg = "added Category Lookup row:#{row[:id]} - #{rec[:id]},#{rec[:lu_code]}, #{rec[:lu_desc]}"
     end
-    diff_num_recs = LookupTable.all.count - start_rec_count
-    msg = "# imported #{diff_num_recs} new records from WWEIA Categories"
-    Rails.logger.info("***#{msg}")
-    @report << msg
-    Rails.logger.error("ERROR: #{@errors.inspect}") if @errors.count > 0
+    if rec.errors.count > 0
+      msg = "Error writing USDA Category table row:#{row[:id]} - #{rec[:id]} - #{rec.errors.full_messages}"
+      err_str = msg
+    end
+    return msg, err_str
   end
+
+
+  # callback method to set the wweia food category entries in the Lookup Table
+  #
+  # @param rec is the current record in LookupTables
+  # @param row is the current row being read from the csv file
+  # @return - [ <the message to go in the report>, <the optional error message, or blank> ]
+  # @example
+  #   see run() method for examples
+  def set_wweia_category_fields(rec, row)
+    Rails.logger.debug "*** WWEIA Food Category Record:"
+    Rails.logger.debug "*** rec: #{rec.inspect}"
+    Rails.logger.debug "*** row: #{row.inspect}"
+
+    err_str = msg = ""
+    # put in fields needed for both update and add
+    rec.lu_desc = row[:wweia_food_category_description]
+    # Update the record if it existed already
+    if rec.present? && rec.id.present?
+      if rec.lu_table != 'wweia_cat' || rec.lu_code != row[:code].to_s
+        msg = "Error, trying to update an invalid record at Lookup Table id: #{rec.id}"
+        Rails.logger.error msg
+        Rails.logger.error "### row[:code]: #{row[:code].inspect} rec: #{rec.inspect}"
+        err_str = msg
+      elsif rec.changed
+        rec.save
+        msg = "Updated WWEIA Category Lookup row.id:#{row[:id]} - #{rec[:id]},#{rec[:lu_code]}, #{rec[:lu_desc]}"
+      else
+        # no need to update it, as it has not changed
+      end
+    else
+      # new record, create it anew
+      rec.lu_table = 'wweia_cat'
+      rec.lu_code = row[:wweia_food_category]
+      rec.save
+      msg = "added WWEIA Category Lookup row:#{row[:id]} - #{rec[:id]},#{rec[:lu_code]}, #{rec[:lu_desc]}"
+    end
+    if rec.errors.count > 0
+      msg = "Error writing WWEIA Category table row:#{row[:id]} - #{rec[:id]} - #{rec.errors.full_messages}"
+      err_str = msg
+    end
+    return msg, err_str
+  end
+
 
 end
